bfafc338977990f34e0e371930e5e93a30407449
wangling <wb-wanglingwl@alibaba-inc.com>
2015-04-14 00:59:38 +0800

    fix

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(function() {
-	$('.typing').each(function(i, ele) {
-		ele = $(ele);
-		var length = ele.text().length;
-		ele.parent().css('width', length + 'em');
-		ele.css('-webkit-animation-timing-function', 'steps(' + length + ', end)');
-	});
+	// $('.typing').each(function(i, ele) {
+	// 	ele = $(ele);
+	// 	var length = ele.text().length;
+	// 	ele.parent().css('width', length + 'em');
+	// 	ele.css('-webkit-animation-timing-function', 'steps(' + length + ', end)');
+	// });
 })
