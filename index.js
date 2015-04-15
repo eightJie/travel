31c7b943b0eff728fb3c0f7d88d0ebd837ad01e8
wangling <wb-wanglingwl@alibaba-inc.com>
2015-04-15 23:06:44 +0800

    hor

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 $(function() {
+
+	function PreImgs(imgs){
+		this.imgs = imgs;
+		this.index = 0;
+	}
+	PreImgs.prototype = {
+
+		_load: function(){
+			var self = this;
+			var urls = self.imgs;
+
+			urls.forEach(function(url){
+				var img = new Image();
+
+				img.onload = function(){
+					self.index++;
+					if(self.index == imgs.length){
+						self.loadedBack && self.loadedBack();
+					}
+				};
+				img.src = url;
+
+			});
+		},
+
+		loaded: function(back){
+			this.loadedBack = back;
+		}
+
+	};
+
+
+	
+
+
+
 	// $('.typing').each(function(i, ele) {
 	// 	ele = $(ele);
 	// 	var length = ele.text().length;
